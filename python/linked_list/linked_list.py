aef42b5e50a4cc14561fbd163a21be4d64da24f9
polo <lg550055@hotmail.com>
2022-04-22 18:14:59 -0500

    Adds partial solution to Challenge 05

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,28 @@
+class Node:
+    """ Creates and maintains linked list nodes """
+    def __init__(self, value, next_=None): #using _ to differentiate from built in next method
+        self.value = value
+        self.next = next_
+
 class LinkedList:
     """
     Put docstring here
     """
-
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
+    def insert(self, value):
+        """ inserts new head node """
+        # pass the old head as the next property of the inserted node
+        self.head = Node(value, self.head)
+
+    def includes(self, target):
+        current = self.head
+        while current:
+            if current.value == target:
+                return True
+            current = current.next
+        return False
+
+    def __str__(self):
         pass
