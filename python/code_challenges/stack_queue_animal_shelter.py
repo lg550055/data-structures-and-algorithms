b678eb20950dbd060e0cdc0f72fddd5516f086cb
polo <lg550055@hotmail.com>
2022-05-03 15:30:02 -0500

    Passes all challenge 12 tests

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -1,13 +1,30 @@
-from data_structures.queue import Queue
+from data_structures.stack import Stack
 
 
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def enqueue(self, animal):
+        self.stack1.push(animal)
+
+    def dequeue(self, pref):
+        while not self.stack1.is_empty():
+            self.stack2.push(self.stack1.pop())
+        while not self.stack2.is_empty():
+            if self.stack2.peek().value == pref:
+                return self.stack2.pop()
+            elif not self.stack2.is_empty():
+                self.stack1.push(self.stack2.pop())
+        return
 
 
-class Dog:
-    pass
+class Dog():
+    def __init__(self):
+        self.value = 'dog'
 
 
-class Cat:
-    pass
+class Cat():
+    def __init__(self):
+        self.value = 'cat'
