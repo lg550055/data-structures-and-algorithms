eaa3ba720b1805d3476328384c4005a2182a2687
polo <lg550055@hotmail.com>
2022-05-04 14:55:47 -0500

    Passes all req and added tests

--- python/tests/code_challenges/test_stack_queue_brackets.py
+++ python/tests/code_challenges/test_stack_queue_brackets.py
@@ -2,64 +2,80 @@ import pytest
 from code_challenges.stack_queue_brackets import multi_bracket_validation
 
 
-@pytest.mark.skip("TODO")
 def test_validates_two_square_brackets():
     actual = multi_bracket_validation("[]")
     expected = True
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_fails_two_square_brackets_flipped():
     actual = multi_bracket_validation("][")
     expected = False
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_validates_two_braces():
     actual = multi_bracket_validation("{}")
     expected = True
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_fails_two_braces_flipped():
     actual = multi_bracket_validation("}{")
     expected = False
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_validates_two_parentheses():
     actual = multi_bracket_validation("()")
     expected = True
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_fails_two_parentheses_flipped():
     actual = multi_bracket_validation(")(")
     expected = False
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_multi():
     actual = multi_bracket_validation("{}(){}")
     expected = True
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_nested():
     actual = multi_bracket_validation("{([])}")
     expected = True
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
 def test_mismatched():
     actual = multi_bracket_validation("[}")
+    expected = False
+    assert actual == expected
+
+def test_one_curly_open():
+    actual = multi_bracket_validation("{")
+    expected = False
+    assert actual == expected
+
+def test_one_paren_closed():
+    actual = multi_bracket_validation(")")
+    expected = False
+    assert actual == expected
+
+def test_extra_chars():
+    actual = multi_bracket_validation("()[[Extra Characters]]")
+    expected = True
+    assert actual == expected
+
+def test_extra_chars_apart():
+    actual = multi_bracket_validation("{}{Code}[Fellows](())")
+    expected = True
+    assert actual == expected
+
+def test_closed_wrong_order():
+    actual = multi_bracket_validation("{(})")
     expected = False
     assert actual == expected
