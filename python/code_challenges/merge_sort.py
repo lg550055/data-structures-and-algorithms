a88660214c30b47c9105a4d736cadaf5add1ffa4
polo <lg550055@hotmail.com>
2022-05-24 21:41:58 -0500

    First merge-sort commit

---
+++ python/code_challenges/merge_sort.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+  n = len(arr)
+  print(f'merge_sort {arr}')
+  if n > 1:
+    mid = n // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+    merge(left, right, arr)
+
+def merge(left, right, arr):
+  i = j = k = 0
+  print(f'merge: left {left}, right {right}')
+  while i < len(left) and j < len(right):
+    print(f'selecting smallest of {left[i:]} and {right[j:]}')
+    if left[i] <= right[j]:
+      arr[k] = left[i]
+      i += 1
+    else:
+      arr[k] = right[j]
+      j += 1
+    print(f'appending {arr[k]}')
+    k += 1
+    print(f'results in {arr[:k]}')
+  if i == len(left):
+    for x in range(j, len(right)):
+      print(f'append remaining entry {right[x]}')
+      arr[k] = right[x]
+      k += 1
+  else:
+    for x in range(i, len(left)):
+      print(f'append remaining entry {left[x]}')
+      arr[k] = left[x]
+      k += 1
+  print(f'result of merge {arr}')
+
+if __name__ == '__main__':
+  nums = [8,4,23,42,16,15]
+  merge_sort(nums)
+  print(f'sorted {nums}')
