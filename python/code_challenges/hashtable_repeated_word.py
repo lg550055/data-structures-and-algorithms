b34ed2a66824d94cdf8ddb8d9953a3dd04c8adbf
polo <lg550055@hotmail.com>
2022-05-26 15:46:33 -0500

    Passes required and custom tests

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,11 @@
 from data_structures.hashtable import Hashtable
+import re
 
-
-def first_repeated_word():
-    pass
+def first_repeated_word(text):
+    nopunc = re.sub(r'[,.?!;:-]', '', text).lower()
+    stext = nopunc.split()
+    h = {}
+    for w in stext:
+        if w in h:
+            return w
+        h[w] = 1
